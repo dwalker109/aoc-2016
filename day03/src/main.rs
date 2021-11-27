1de7c2b1d6efee8ab2d16ce11935a589c0077706
Dan Walker <dwalker109@gmail.com>
2021-11-27 03:21:43 +0000

    Day 3: Squares With Three Sides (2/2)

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,24 +1,44 @@
-use itertools::{self, Itertools};
+use itertools::Itertools;
 
 fn main() {
     println!("Part 1: {}", part_1(include_str!("../input")));
+    println!("Part 2: {}", part_2(include_str!("../input")));
 }
 
 fn part_1(input: &'static str) -> u32 {
-    input.lines().fold(0u32, |acc, el| {
-        let is_triangle = el
-            .split_ascii_whitespace()
-            .map(|n| n.parse::<u32>().unwrap())
-            .permutations(3)
-            .all(|p| {
-                let &a = p.get(0).unwrap();
-                let &b = p.get(1).unwrap();
-                let &c = p.get(2).unwrap();
+    input
+        .lines()
+        .map(split_to_u32)
+        .fold(0u32, |acc, el| acc + (is_triangle(el) as u32))
+}
 
-                a + b > c
-            });
+fn part_2(input: &'static str) -> u32 {
+    let col_chunked = split_to_u32(input).chunks(9);
 
-        acc + (is_triangle as u32)
+    let reordered = col_chunked.into_iter().flat_map(|c| {
+        let v = c.collect_vec();
+        let [a1, b1, c1, a2, b2, c2, a3, b3, c3] = <[u32; 9]>::try_from(v).unwrap();
+        vec![
+            vec![a1, a2, a3].into_iter(),
+            vec![b1, b2, b3].into_iter(),
+            vec![c1, c2, c3].into_iter(),
+        ]
+    });
+
+    reordered.fold(0u32, |acc, el| acc + (is_triangle(el) as u32))
+}
+
+fn split_to_u32(input: &'static str) -> impl Iterator<Item = u32> {
+    input
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<u32>().unwrap())
+}
+
+fn is_triangle(maybe_triangle: impl Iterator<Item = u32>) -> bool {
+    maybe_triangle.permutations(3).all(|p| {
+        let [a, b, c] = <[u32; 3]>::try_from(p).unwrap();
+
+        a + b > c
     })
 }
 
@@ -26,7 +46,13 @@ fn part_1(input: &'static str) -> u32 {
 mod tests {
     #[test]
     fn part_1() {
-        let r = super::part_1(include_str!("../input_test"));
+        let r = super::part_1(include_str!("../input"));
         assert_eq!(r, 917)
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2(include_str!("../input"));
+        assert_eq!(r, 1649)
     }
 }
