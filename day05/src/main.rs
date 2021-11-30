838748b6a80977afe351acb2f8ae3974752cc94d
Dan Walker <dwalker109@gmail.com>
2021-11-30 02:49:16 +0000

    Day 5: How About a Nice Game of Chess? (2/2)

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,7 +1,13 @@
 use md5::{Digest, Md5};
+use std::fmt::Write;
+use std::thread;
 
 fn main() {
-    println!("Part 1: {}", part_1("ojvtpuvg"));
+    let h1 = thread::spawn(move || part_1("ojvtpuvg"));
+    let h2 = thread::spawn(move || part_2("ojvtpuvg"));
+
+    println!("Part 1: {}", h1.join().unwrap());
+    println!("Part 2: {}", h2.join().unwrap());
 }
 
 fn part_1(input: &'static str) -> String {
@@ -10,20 +16,16 @@ fn part_1(input: &'static str) -> String {
     let mut hasher = Md5::new();
 
     for i in 0.. {
-        let door_id_and_index = &[input, i.to_string().as_bytes()].concat();
-        let needle = String::from_utf8_lossy(door_id_and_index);
-        hasher.update(needle.as_bytes());
-        let res = hasher.finalize_reset();
+        let res = next_hash(&mut hasher, &input, &i);
 
-        let first_4_hex_as_u8 = <[u8; 2]>::try_from(&res.as_slice()[..2]).unwrap();
-        if first_4_hex_as_u8 == [0, 0] {
-            let next_2_hex_as_u8 = &res.as_slice()[2];
-            let next_2_hex_formatted = format!("{:02x}", next_2_hex_as_u8);
-            let mut maybe_match = next_2_hex_formatted.chars();
-            if maybe_match.next().unwrap() == '0' {
-                password.push(maybe_match.next().unwrap());
-            }
+        if res[..2] != [0, 0] {
+            continue;
         }
+        if res[2] >= 16 {
+            continue;
+        }
+
+        password.push(std::char::from_digit(res[2] as u32, 16).unwrap());
 
         if password.len() == 8 {
             return password.into_iter().collect();
@@ -33,11 +35,61 @@ fn part_1(input: &'static str) -> String {
     panic!();
 }
 
+fn part_2(input: &'static str) -> String {
+    let input = input.as_bytes();
+    let mut password = vec![None; 8];
+    let mut next_hex_digits = String::with_capacity(4);
+    let mut hasher = Md5::new();
+
+    for i in 0.. {
+        let res = next_hash(&mut hasher, &input, &i);
+
+        if res[..2] != [0, 0] {
+            continue;
+        }
+        if res[2] >= 16 {
+            continue;
+        }
+
+        next_hex_digits.clear();
+        write!(next_hex_digits, "{:x}{:02x}", res[2], res[3]).unwrap();
+        let mut maybe_match = next_hex_digits.chars();
+
+        let pos = match maybe_match.next().unwrap().to_string().parse::<u8>() {
+            Ok(pos) => pos as usize,
+            Err(_) => continue,
+        };
+
+        if pos < 8 && password[pos] == None {
+            password[pos] = maybe_match.next();
+
+            if !password.contains(&None) {
+                return password.into_iter().flatten().collect();
+            }
+        }
+    }
+
+    panic!();
+}
+
+fn next_hash(hasher: &mut Md5, input: &[u8], i: &usize) -> [u8; 16] {
+    let needle = &[input, i.to_string().as_bytes()].concat();
+    hasher.update(needle);
+
+    hasher.finalize_reset().into()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn part_1() {
-        let r = super::part_1("abc");
-        assert_eq!(r, "18f47a30");
+        let r = super::part_1("ojvtpuvg");
+        assert_eq!(r, "4543c154");
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2("ojvtpuvg");
+        assert_eq!(r, "1050cbbd");
     }
 }
