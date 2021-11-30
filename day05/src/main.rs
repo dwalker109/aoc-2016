a831958664994611c4786cc3ff0132552637d858
Dan Walker <dwalker109@gmail.com>
2021-11-30 02:49:16 +0000

    Day 5: How About a Nice Game of Chess? (2/2)

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -2,6 +2,7 @@ use md5::{Digest, Md5};
 
 fn main() {
     println!("Part 1: {}", part_1("ojvtpuvg"));
+    println!("Part 2: {}", part_2("ojvtpuvg"));
 }
 
 fn part_1(input: &'static str) -> String {
@@ -10,16 +11,14 @@ fn part_1(input: &'static str) -> String {
     let mut hasher = Md5::new();
 
     for i in 0.. {
-        let door_id_and_index = &[input, i.to_string().as_bytes()].concat();
-        let needle = String::from_utf8_lossy(door_id_and_index);
-        hasher.update(needle.as_bytes());
+        let needle = &[input, i.to_string().as_bytes()].concat();
+        hasher.update(needle);
         let res = hasher.finalize_reset();
 
-        let first_4_hex_as_u8 = <[u8; 2]>::try_from(&res.as_slice()[..2]).unwrap();
-        if first_4_hex_as_u8 == [0, 0] {
-            let next_2_hex_as_u8 = &res.as_slice()[2];
-            let next_2_hex_formatted = format!("{:02x}", next_2_hex_as_u8);
-            let mut maybe_match = next_2_hex_formatted.chars();
+        if res[..2] == [0, 0] {
+            let next_hex_digit = format!("{:02x}", res[2]);
+            let mut maybe_match = next_hex_digit.chars();
+
             if maybe_match.next().unwrap() == '0' {
                 password.push(maybe_match.next().unwrap());
             }
@@ -33,11 +32,56 @@ fn part_1(input: &'static str) -> String {
     panic!();
 }
 
+fn part_2(input: &'static str) -> String {
+    let input = input.as_bytes();
+    let mut password = vec![None; 8];
+    let mut hasher = Md5::new();
+
+    for i in 0.. {
+        let needle = &[input, i.to_string().as_bytes()].concat();
+
+        hasher.update(&needle);
+        let res = hasher.finalize_reset();
+
+        if res[..2] != [0, 0] {
+            continue;
+        }
+
+        let next_hex_digits = &format!("{:02x}{:02x}", res[2], res[3]);
+        let mut maybe_match = next_hex_digits.chars();
+
+        if maybe_match.next().unwrap() != '0' {
+            continue;
+        }
+
+        let pos = match maybe_match.next().unwrap().to_string().parse::<u8>() {
+            Ok(pos) => pos as usize,
+            Err(_) => continue,
+        };
+
+        if pos < 8 && password[pos] == None {
+            password[pos] = maybe_match.next();
+
+            if !password.contains(&None) {
+                return password.into_iter().flatten().collect();
+            }
+        }
+    }
+
+    panic!();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn part_1() {
-        let r = super::part_1("abc");
-        assert_eq!(r, "18f47a30");
+        let r = super::part_1("ojvtpuvg");
+        assert_eq!(r, "4543c154");
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2("ojvtpuvg");
+        assert_eq!(r, "1050cbbd");
     }
 }
