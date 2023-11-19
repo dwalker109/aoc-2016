4aa75dc8fc5b57404e5cad06462546d58a2500b8
Dan Walker <dwalker109@gmail.com>
2023-11-19 02:18:59 +0000

    Day 6: Signals and Noise (1 & 2)

--- days/day06/src/main.rs
+++ days/day06/src/main.rs
@@ -1,15 +1,57 @@
+use itertools::Itertools;
+
 static INPUT: &str = include_str!("../input");
 
 fn main() {
     aoc_shared::runner::solve(|| part1(INPUT), || part2(INPUT))
 }
 
-fn part1(input: &'static str) -> usize {
-    todo!()
+fn part1(input: &'static str) -> String {
+    let w = input.lines().next().unwrap().len();
+
+    input
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .enumerate()
+        .fold(vec![Vec::new(); w], |mut acc, (i, c)| {
+            acc[i % w].push(c);
+            acc
+        })
+        .iter()
+        .map(|v| {
+            v.iter()
+                .counts()
+                .iter()
+                .max_by_key(|x| x.1)
+                .unwrap()
+                .0
+                .to_owned()
+        })
+        .collect()
 }
 
-fn part2(input: &'static str) -> usize {
-    todo!()
+fn part2(input: &'static str) -> String {
+    let w = input.lines().next().unwrap().len();
+
+    input
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .enumerate()
+        .fold(vec![Vec::new(); w], |mut acc, (i, c)| {
+            acc[i % w].push(c);
+            acc
+        })
+        .iter()
+        .map(|v| {
+            v.iter()
+                .counts()
+                .iter()
+                .min_by_key(|x| x.1)
+                .unwrap()
+                .0
+                .to_owned()
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -18,11 +60,11 @@ mod tests {
 
     #[test]
     fn part_1() {
-        assert_eq!(super::part1(INPUT), 0);
+        assert_eq!(super::part1(INPUT), "easter");
     }
 
     #[test]
     fn part_2() {
-        assert_eq!(super::part2(INPUT), 0);
+        assert_eq!(super::part2(INPUT), "advent");
     }
 }
