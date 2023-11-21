2583e9a9faa8e18b7fb02514b582aad68cde3967
Dan Walker <dwalker109@gmail.com>
2023-11-21 01:49:19 +0000

    Day 7: Internet Protocol Version 7 (1 & 2)

--- days/day07/src/main.rs
+++ days/day07/src/main.rs
@@ -1,30 +1,93 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
 static INPUT: &str = include_str!("../../../input/day07");
 
-type Answer = aoc_shared::runner::AnswerTbd;
+type Answer = usize;
 
 fn main() {
     aoc_shared::runner::solve(|| part1(INPUT), || part2(INPUT))
 }
 
 fn part1(input: &'static str) -> Answer {
-    todo!();
+    input
+        .lines()
+        .filter_map(|l| {
+            let mut hn_active = false;
+            let mut abba = None;
+
+            for (l1, l, r, r1) in l.chars().tuple_windows() {
+                match l1 {
+                    '[' => hn_active = true,
+                    ']' => hn_active = false,
+                    _ => match l1 == r1 && l == r && l1 != l && r != r1 {
+                        true if hn_active => {
+                            abba = None;
+                            break;
+                        }
+                        true if !hn_active => {
+                            abba = Some(());
+                        }
+                        _ => continue,
+                    },
+                }
+            }
+
+            abba
+        })
+        .count()
 }
 
 fn part2(input: &'static str) -> Answer {
-    todo!();
+    #[derive(Eq, PartialEq)]
+    enum Mode {
+        Aba,
+        Bab,
+    }
+
+    input
+        .lines()
+        .filter_map(|l| {
+            let mut mode = Mode::Aba;
+            let mut aba = HashSet::new();
+            let mut bab = HashSet::new();
+
+            for (l, m, r) in l.chars().tuple_windows() {
+                match l {
+                    '[' => mode = Mode::Bab,
+                    ']' => mode = Mode::Aba,
+                    _ => match l == r && l != m {
+                        true if mode == Mode::Aba => {
+                            aba.insert((l, m, r));
+                        }
+                        true if mode == Mode::Bab => {
+                            bab.insert((l, m, r));
+                        }
+                        _ => continue,
+                    },
+                }
+            }
+
+            aba.iter()
+                .any(|(l, m, _r)| bab.contains(&(*m, *l, *m)))
+                .then_some(())
+        })
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
-    static INPUT: &str = include_str!("../input_test");
+    static INPUT_1: &str = include_str!("../input_test_1");
 
     #[test]
     fn part1() {
-        assert_eq!(super::part1(INPUT), super::Answer::default());
+        assert_eq!(super::part1(INPUT_1), 2);
     }
+
+    static INPUT_2: &str = include_str!("../input_test_2");
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(INPUT), super::Answer::default());
+        assert_eq!(super::part2(INPUT_2), 3);
     }
 }
