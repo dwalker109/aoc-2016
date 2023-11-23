d29f863cc9edf8eb42beecd7c0d3ef4d65813f31
Dan Walker <dwalker109@gmail.com>
2023-11-23 23:28:18 +0000

    Wiiip

--- days/day08/src/main.rs
+++ days/day08/src/main.rs
@@ -1,3 +1,6 @@
+use nom::character::complete::digit1;
+use nom::combinator::map_res;
+use nom::sequence::tuple;
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -17,8 +20,9 @@ fn main() {
     aoc_shared::runner::solve(|| part1(INPUT), || part2(INPUT))
 }
 
-fn part1(input: &'static str) -> Answer {
-    todo!();
+const fn part1<const WIDTH: usize, const HEIGHT: usize>(input: &'static str) -> Answer {
+    let screen = Screen(HashSet::new());
+    let commands = input.lines().map(Op::from).collect::<Vec<_>>();
 }
 
 fn part2(input: &'static str) -> Answer {
@@ -26,10 +30,22 @@ fn part2(input: &'static str) -> Answer {
 }
 
 #[derive(Debug, Default)]
-struct Screen {
-    pixels: HashSet<usize, usize>,
-    width: usize,
-    height: usize,
+struct Screen(HashSet<(usize, usize)>);
+
+impl Screen {
+    pub fn act<const WIDTH: usize, const HEIGHT: usize>(&mut self, op: &Op) {
+        match op {
+            Op::Rect(width, height) => {
+                for x in 0..*width {
+                    for y in 0..*height {
+                        self.0.insert((x, y));
+                    }
+                }
+            }
+            Op::Row(y, n) => {}
+            Op::Col(x, n) => {}
+        }
+    }
 }
 
 enum Op {
@@ -40,36 +56,48 @@ enum Op {
 
 impl From<&str> for Op {
     fn from(input: &str) -> Self {
-        // fn rect(input: &str) -> IResult<&str, Op> {
-        //     map(
-        //         (tag("rect "), separated_pair(be_u8, char('x'), be_u8)),
-        //         |(_, x, y)| Op::Rect(x, y),
-        //     )(input)
-        // }
-
-        let (_, op) = alt((
-            map(
-                (tag("rect "), separated_pair(be_u8, char('x'), be_u8)),
-                |(_, x, y)| Op::Rect(x, y),
-            ),
-            map(
-                (
-                    tag("rotate row y="),
-                    separated_pair(be_u8, tag(" by "), be_u8),
+        let rect = |input: &str| -> IResult<&str, Op> {
+            let (input, (_, (x, y))) = tuple((
+                tag("rect "),
+                separated_pair(
+                    map_res(digit1, str::parse),
+                    char('x'),
+                    map_res(digit1, str::parse),
                 ),
-                |(_, y, d)| Op::Row(y, d),
-            ),
-            map(
-                (
-                    tag("rotate column x="),
-                    separated_pair(be_u8, tag(" by "), be_u8),
-                ),
-                |(_, y, d)| Op::Col(y, d),
-            ),
-        ))(input)
-        .unwrap();
+            ))(input)?;
 
-        todo!()
+            Ok((input, Op::Rect(x, y)))
+        };
+
+        let row = |input: &str| -> IResult<&str, Op> {
+            let (input, (_, (y, n))) = tuple((
+                tag("rotate row y= "),
+                separated_pair(
+                    map_res(digit1, str::parse),
+                    tag(" by "),
+                    map_res(digit1, str::parse),
+                ),
+            ))(input)?;
+
+            Ok((input, Op::Row(y, n)))
+        };
+
+        let col = |input: &str| -> IResult<&str, Op> {
+            let (input, (_, (x, n))) = tuple((
+                tag("rotate column x= "),
+                separated_pair(
+                    map_res(digit1, str::parse),
+                    tag(" by "),
+                    map_res(digit1, str::parse),
+                ),
+            ))(input)?;
+
+            Ok((input, Op::Col(x, n)))
+        };
+
+        let (_, op) = alt((rect, row, col))(input).unwrap();
+
+        op
     }
 }
 
