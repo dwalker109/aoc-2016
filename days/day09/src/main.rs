fa0a87d3b285a7d66e84b4d1e07ba47abac92472
Dan Walker <dwalker109@gmail.com>
2023-11-28 23:32:47 +0000

    Day 9: Explosives in Cyberspace (2)

--- days/day09/src/main.rs
+++ days/day09/src/main.rs
@@ -17,33 +17,58 @@ fn main() {
 }
 
 fn part1(input: &'static str) -> Answer {
+    unexploder(input)
+}
+
+fn part2(input: &'static str) -> Answer {
+    exploder(input).1
+}
+
+fn unexploder(input: &str) -> usize {
     let mut input = input;
-    let mut output = String::new();
+    let mut n = 0;
 
     while !input.is_empty() {
-        let (rest_input, (verbatim, (seq, reps))) = consume_to_marker(input).unwrap();
+        let (rest_input, (literal, marker)) = next_tokens(input).unwrap();
         input = rest_input;
 
-        output.push_str(verbatim);
+        n += literal.unwrap_or_default().len();
 
-        let (rest_input, next_seq) = take::<_, _, Error<_>>(seq)(input).unwrap();
-        input = rest_input;
+        if let Some((seq, reps)) = marker {
+            let (rest_input, next_seq) = take::<_, _, Error<_>>(seq)(input).unwrap();
+            input = rest_input;
 
-        for _ in 0..reps {
-            output.push_str(next_seq);
+            n += reps * next_seq.len();
         }
     }
 
-    output.len()
+    n
 }
 
-fn part2(_input: &'static str) -> Answer {
-    todo!();
+fn exploder(mut input: &str) -> (&str, usize) {
+    let (rest_input, (literal, marker)) = next_tokens(input).unwrap();
+    input = rest_input;
+
+    let mut n = literal.unwrap_or_default().len();
+
+    if let Some((len, rep)) = marker {
+        let (_, inner_n) = exploder(&input[..len]);
+        input = &input[len..];
+        n += rep * inner_n;
+    }
+
+    if !input.is_empty() {
+        let (rest_input, inner_n) = exploder(input);
+        input = rest_input;
+        n += inner_n;
+    }
+
+    (input, n)
 }
 
-fn consume_to_marker(input: &str) -> IResult<&str, (&str, (usize, usize))> {
-    let (input, (verbatim, _, decompression)) = tuple((
-        alphanumeric0,
+fn next_tokens(input: &str) -> IResult<&str, (Option<&str>, Option<(usize, usize)>)> {
+    let (input, (literal, _, marker)) = tuple((
+        opt(alphanumeric0),
         opt(newline),
         opt(delimited(
             char('('),
@@ -56,7 +81,7 @@ fn consume_to_marker(input: &str) -> IResult<&str, (&str, (usize, usize))> {
         )),
     ))(input)?;
 
-    Ok((input, (verbatim, decompression.unwrap_or_default())))
+    Ok((input, (literal, marker)))
 }
 
 #[cfg(test)]
@@ -75,6 +100,12 @@ mod tests {
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(INPUT), super::Answer::default());
+        assert_eq!(super::part2("(3x3)XYZ"), 9);
+        assert_eq!(super::part2("X(8x2)(3x3)ABCY"), 20);
+        assert_eq!(super::part2("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
+        assert_eq!(
+            super::part2("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"),
+            445
+        );
     }
 }
